8f976cb32e25dcdeb6ea523ccac20abf26eb7a87
Brendan Ahearn <91020994+Arcanaut@users.noreply.github.com>
2021-11-01 17:34:23 -0500

    mid-fix of merge conflicts

--- assets/js/game.js
+++ assets/js/game.js
@@ -64,6 +64,73 @@ var fight = function(enemyName) {
   }
 };
 
+<<<<<<< Updated upstream
+=======
+
+// function to set name
+var getPlayerName = function() {
+  var name = "";
+
+  while (name === "" || name === null) {
+    name = prompt("What is your robot's name?");
+  }
+  console.log("Your robot's name is " + name);
+  return name;
+};
+//end of name function
+
+
+//game info and variables
+var playerInfo = {
+  name: getPlayerName(), 
+  health: 100,
+  attack: 10,
+  money: 10,
+  reset: function () {
+    this.health = 100;
+    this.money = 10;
+    this.attack = 10;
+  }, // comma!
+
+  refillHealth: function () {
+    if (this.money >= 7) {
+      window.alert("Refilling player's health by 20 for 7 dollars.");
+      this.health += 20;
+      this.money -= 7;
+    } else {
+      window.alert("You don't have enough money!");
+    }
+  },
+  upgradeAttack: function () {
+    if (this.money >= 7) {
+      window.alert("Upgrading player's attack by 6 for 7 dollars.");
+      this.attack += 6;
+      this.money -= 7;
+    } else {
+      window.alert("You don't have enough money!");
+    }
+  }
+};
+
+//enemy info
+var enemyInfo = [{
+    name: "Roborto",
+    attack: randomNumber(10, 14)
+  },
+  {
+    name: "Amy Android",
+    attack: randomNumber(10, 14)
+  },
+  {
+    name: "Robo Trumble",
+    attack: randomNumber(10, 14)
+  }
+];
+
+//end of game info and variables
+
+
+>>>>>>> Stashed changes
 // function to start a new game
 var startGame = function() {
   // reset player stats
